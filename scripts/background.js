405444df0ad4b8369688350571fe0239392cae52
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-07-28 13:02:29 +0200

    feat(saveCode): move logic to render saveCode popup to backgroung.js and store code details using chrome storage API

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,35 @@
+
 // listen for messages sended from content scripts
-chrome.runtime.onMessage.addListener((message, sender, response) => {
+chrome.runtime.onMessage.addListener( async (message, sender, response) => {
+  await chrome.storage.sync.set({ code: message.code });
+  await chrome.storage.sync.set({ title: message.pageTitle });
   if (message.saveButtonIsClicked === true) {
     openModal()
-    response({ status: "saving" })
+    response({ status: "ok" })
   }
 });
 
-//function to open iframe in Modal
+// function to render popup with a form to save code
+function renderPopup() {
+
+  const modal = document.createElement("dialog");
+  modal.className = 'popup-dialog'
+  modal.innerHTML = `<iframe class="popup-iframe" id="popup-content"></iframe>
+  <button class="close-btn"><img src=`+ chrome.runtime.getURL("/images/icons/close.png") +` alt="close window"></button>`;
+  document.body.appendChild(modal);
+  const dialog = document.querySelector("dialog");
+  dialog.showModal();
+
+  const iframe = document.getElementById("popup-content");
+  iframe.src = chrome.runtime.getURL("/pages/saveCode.html");
+  iframe.frameBorder = 0;
+
+  dialog.querySelector(".close-btn").addEventListener("click", () => {
+  dialog.close();
+  })
+}
+
+//function to open iframe
 function openModal() {
   chrome.tabs.query({ //ask for tab infos
     currentWindow: true,
@@ -14,10 +37,8 @@ function openModal() {
   })
     .then(([tab]) => { //callback function to inject script into current tab
       chrome.scripting.executeScript({
-        target: {
-          tabId: tab.id
-        },
-        files: ["/scripts/saveCode.js"]
+        target: { tabId: tab.id },
+        func: renderPopup,
       });
     });
 };
--- scripts/saveCode.js
+++ scripts/saveCode.js
@@ -1,18 +1,11 @@
-// render popup modal
-window.onload = renderPopup();
-
-function renderPopup() {
-    const modal = document.createElement("dialog");
-    modal.className = 'popup-dialog'
-    modal.innerHTML = `<iframe class="popup-iframe" id="popup-content"></iframe>
-    <button class="close-btn"><img src=`+chrome.runtime.getURL("/images/icons/close.png")+`></button>`;
-    document.body.appendChild(modal);
-    const dialog = document.querySelector("dialog");
-    dialog.showModal();
-    const iframe = document.getElementById("popup-content");
-    iframe.src = chrome.runtime.getURL("/pages/saveCode.html");
-    iframe.frameBorder = 0;
-    dialog.querySelector(".close-btn").addEventListener("click", () => {
-    dialog.close();
-    })
+// function to fill the form with code information recived
+function updateFormInputs(code, title) {
+    const inputCode = document.getElementById('codeSnippet')
+    inputCode.innerHTML = code
+    const inputTitle = document.getElementById('codeTitle')
+    inputTitle.value = title
 }
+
+chrome.storage.sync.get(['code', 'title'], ({ code, title }) => {
+    updateFormInputs(code, title)
+});
